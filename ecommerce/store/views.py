eae3f240bbae514e528ab863502d4e93cbb58fa0
Cierra Johnson-carter <cierrajohnson-carter@Cierras-Air.lan1>
2020-05-26 18:00:56 -0400

    created path for new view

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import *
 
 # Create function-based views.
@@ -38,3 +39,7 @@ def checkout(request):
 
     context = {'items': items, 'order': order}
     return render(request, 'store/checkout.html', context)
+
+
+def updateItem(request):
+    return JsonResponse('Item was added', safe=False)
