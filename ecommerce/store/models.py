6226e6e7a50efb083777e6fdec1eee2748b9fedf
Cierra Johnson-carter <cierrajohnson-carter@Cierras-Air.lan1>
2020-05-26 21:50:02 -0400

    added logic for shipping method

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -43,6 +43,18 @@ class Order(models.Model):
         return str(self.id)
 
     @property
+    def shipping(self):
+        orderitems = self.orderitem_set.all()
+
+        for i in orderitems:
+            if i.product.digital == False:
+                shipping = True
+
+        shipping = False
+
+        return shipping
+
+    @property
     def get_cart_total(self):
         orderitems = self.orderitem_set.all()
         total = sum([item.get_total for item in orderitems])
