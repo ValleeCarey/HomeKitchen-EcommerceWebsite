c52ceead89b2cf1585dc42cf345b749a22aac92a
Cierra Johnson-carter <cierrajohnson-carter@Cierras-Air.lan1>
2020-05-27 20:44:38 -0400

    built order totals and items queryset

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -1,12 +1,12 @@
 var updateBtns = document.getElementsByClassName("update-cart");
 
-for (var i = 0; i < updateBtns.length; i++) {
+for (i = 0; i < updateBtns.length; i++) {
   updateBtns[i].addEventListener("click", function () {
     var productId = this.dataset.product;
     var action = this.dataset.action;
-    console.log(("productId:", productId, "action:", action));
-
+    console.log("productId:", productId, "Action:", action);
     console.log("USER:", user);
+
     if (user == "AnonymousUser") {
       addCookieItem(productId, action);
     } else {
@@ -15,33 +15,8 @@ for (var i = 0; i < updateBtns.length; i++) {
   });
 }
 
-// Function to get cookie item and add/remove products to the cart for an anonymous user
-function addCookieItem(productId, action) {
-  console.log("Not logged in...");
-
-  if (action == "add") {
-    if (cart[productId] == undefined) {
-      cart[productId] = { quantity: 1 };
-    } else {
-      cart[productId]["quantity"] += 1;
-    }
-  }
-
-  if (action == "remove") {
-    cart[productId]["quantity"] -= 1;
-
-    if (cart[productId]["quantity"] <= 0) {
-      console.log("Remove Item");
-      delete cart[productId];
-    }
-  }
-  console.log("cart:", cart);
-  document.cookie = "cart=" + JSON.stringify(cart) + ";domain=;path=/";
-  location.reload();
-}
-
 function updateUserOrder(productId, action) {
-  console.log("User is logged in, sending data...");
+  console.log("User is authenticated, sending data...");
 
   var url = "/update_item/";
 
@@ -56,9 +31,32 @@ function updateUserOrder(productId, action) {
     .then((response) => {
       return response.json();
     })
-
     .then((data) => {
-      console.log(("Data:", data));
       location.reload();
     });
+}
+
+function addCookieItem(productId, action) {
+  console.log("User is not authenticated");
+
+  if (action == "add") {
+    if (cart[productId] == undefined) {
+      cart[productId] = { quantity: 1 };
+    } else {
+      cart[productId]["quantity"] += 1;
+    }
+  }
+
+  if (action == "remove") {
+    cart[productId]["quantity"] -= 1;
+
+    if (cart[productId]["quantity"] <= 0) {
+      console.log("Item should be deleted");
+      delete cart[productId];
+    }
+  }
+  console.log("CART:", cart);
+  document.cookie = "cart=" + JSON.stringify(cart) + ";domain=;path=/";
+
+  location.reload();
 }
